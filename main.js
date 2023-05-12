3a38a76406734fb87dbb98b451fc39391556f846
ilushacomeback <ilya.rudenko.2105@mail.ru>
2023-05-12 14:33:12 +0300

    delete swipper >768px

--- main.js
+++ main.js
@@ -1,8 +1,19 @@
-mySwiper = new Swiper ('.brands', {
+const mySwiper = new Swiper ('.brands', {
     slidesPerView: 'auto',
     refresh: true, 
     pagination: {
         el: '.swiper-pagination',
         clickable: true,
-    },
+    } 
 });
+
+const brands = document.querySelector('.brands')
+console.log(brands)
+window.addEventListener('resize', function(e) {
+    const width = window.innerWidth
+    console.log(width)
+    if (width > 767) {
+        brands.remove()
+    }
+})
+
