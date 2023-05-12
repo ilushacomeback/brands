3ec301dc60d1bbb939e59c14ebd4b7e0c8b13350
ilushacomeback <ilya.rudenko.2105@mail.ru>
2023-05-12 17:14:24 +0300

    add button

--- main.js
+++ main.js
@@ -8,12 +8,30 @@ const mySwiper = new Swiper ('.brands', {
 });
 
 const brands = document.querySelector('.brands')
-console.log(brands)
 window.addEventListener('resize', function(e) {
     const width = window.innerWidth
-    console.log(width)
     if (width > 767) {
         brands.remove()
     }
+})
+
+window.addEventListener('DOMContentLoaded', function(e) {
+    const width = window.innerWidth
+    if (width > 767) {
+        brands.remove()
+    }
+})
+
+const button = document.querySelector('.button')
+const next = document.querySelector('.brands-next-list')
+button.addEventListener('click', function () {
+    console.log(button)
+    next.classList.toggle('brands-next-list1')
+} )
+
+const btn = document.querySelector('.button > span');
+btn.addEventListener('click', function() {
+  btn.innerHTML =
+    (btn.innerHTML === 'Показать всё') ? btn.innerHTML = 'Скрыть' : btn.innerHTML = 'Показать всё';
 })
 
