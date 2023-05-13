657cafa2e8eab805d85e784e8d5dd5f35dad56dc
ilushacomeback <ilya.rudenko.2105@mail.ru>
2023-05-13 18:58:23 +0300

    add .button1

--- main.js
+++ main.js
@@ -1,41 +1,46 @@
-const mySwiper = new Swiper ('.brands', {
-    slidesPerView: 'auto',
-    refresh: true, 
-    pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-    } 
+const mySwiper = new Swiper(".brands", {
+  slidesPerView: "auto",
+  refresh: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
 });
 
-const brands = document.querySelector('.brands')
-window.addEventListener('resize', function(e) {
-    const width = window.innerWidth
-    if (width > 767) {
-        brands.remove()
-    }
-})
+const brands = document.querySelector(".brands");
+window.addEventListener("resize", function (e) {
+  const width = window.innerWidth;
+  if (width > 767) {
+    brands.remove();
+  }
+});
 
-window.addEventListener('DOMContentLoaded', function(e) {
-    const width = window.innerWidth
-    if (width > 767) {
-        brands.remove()
-    }
-})
+window.addEventListener("DOMContentLoaded", function (e) {
+  const width = window.innerWidth;
+  if (width > 767) {
+    brands.remove();
+  }
+});
 
-const button = document.querySelector('.button')
-const next = document.querySelector('.brands-next-list')
-button.addEventListener('click', function () {
-    next.classList.toggle('brands-next-list1')
-} )
+const button = document.querySelector(".button");
+const next = document.querySelector(".brands-next-list");
+button.addEventListener("click", function () {
+  next.classList.toggle("brands-next-list1");
+});
 
-const btn = document.querySelector('.button > span');
-btn.addEventListener('click', function() {
+button.addEventListener("click", function () {
+  button.classList.toggle("button1");
+});
+
+const btn = document.querySelector(".button > span");
+btn.addEventListener("click", function () {
   btn.innerHTML =
-    (btn.innerHTML === 'Показать всё') ? btn.innerHTML = 'Скрыть' : btn.innerHTML = 'Показать всё';
-})
+    btn.innerHTML === "Показать всё"
+      ? (btn.innerHTML = "Скрыть")
+      : (btn.innerHTML = "Показать всё");
+});
 
-const next_1120px = document.querySelector('.brands-next-list_1120px')
-button.addEventListener('click', function () {
-    next_1120px.classList.toggle('brands-next-list_1120px1')
-})
-
+const next_1120px = document.querySelector(".brands-next-list_1120px");
+button.addEventListener("click", function () {
+  next_1120px.classList.toggle("brands-next-list_1120px1");
+});
